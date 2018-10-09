01649534741bf1f91d5c3ab6fe2c1d1f68be26e4
David Thomas <thodl15@gmail.com>
2018-10-09 15:31:58 -0400

    Added store context for the application.

--- src/App.js
+++ src/App.js
@@ -1,33 +1,42 @@
 import React, { Component } from 'react';
 
+import { createStore } from 'redux';
+import { Provider    } from 'react-redux';
+
+import paramsState from './Quotes-SPA/Redux/Reducers/QuoteReducers';
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 import OptionActionsLogic from './Quotes-SPA/Components/OptionActionsLogic';
-import OptionValuesLogic  from './Quotes-SPA/Components/OptionValuesLogic';
+import OptionValuesStore  from './Quotes-SPA/Components/OptionValuesLogic';
 import QuoteTableLogic    from './Quotes-SPA/Components/QuoteTableLogic';
 
 import {Container} from 'reactstrap';
 
+const store = createStore(paramsState);
+
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        {/* 
-          General structure:
-            1. OuterOptionWrapper
-              1.A OptionValues
-              1.B OptionActions
-            2. OuterTableWrapper
-              2.A TableObject
-        */}
-        <Container>
-          <OptionValuesLogic/>
-          <OptionActionsLogic/>
-          <br/>
-          <QuoteTableLogic/>
-        </Container>
-      </div>
+      <Provider store={store}>
+        <div className="App">
+          {/* 
+            General structure:
+              1. OuterOptionWrapper
+                1.A OptionValues
+                1.B OptionActions
+              2. OuterTableWrapper
+                2.A TableObject
+          */}
+          <Container>
+            <OptionValuesStore/>
+            <OptionActionsLogic/>
+            <br/>
+            <QuoteTableLogic/>
+          </Container>
+        </div>
+      </Provider>
     );
   }
 }
