c631995bc0374ea628b3791b60ea75e475ef1996
David Thomas <thodl15@gmail.com>
2018-10-09 11:45:32 -0400

    Added QuoteTable logic component framework.

---
+++ src/Quotes-SPA/Components/QuoteTableLogic.js
@@ -0,0 +1,21 @@
+// ----------------------------------------------------------------------------
+// Core Imports:
+
+// React Imports:
+import React from 'react';
+
+// Structure Import:
+import QuoteTableStruct from './QuoteTableStruct';
+
+
+// ----------------------------------------------------------------------------
+// Module Exports:
+export default class QuoteTableLogic extends React.Component {
+    render() {
+        return (
+            <QuoteTableStruct
+
+            />
+        )
+    }
+}
