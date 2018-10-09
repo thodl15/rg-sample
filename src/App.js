5279f710615d62f9bed963c0b830221d62b70dee
David Thomas <thodl15@gmail.com>
2018-10-09 10:33:33 -0400

    Replaced sample code with inital app structure.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+
+import OptionActionsStruct from './Quotes-SPA/Components/OptionActionsStruct';
+import OptionValuesStruct  from './Quotes-SPA/Components/OptionValuesStruct';
+import QuoteTableStruct from './Quotes-SPA/Components/QuoteTableStruct';
+
+import {Container} from 'reactstrap';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        {/* 
+          General structure:
+            1. OuterOptionWrapper
+              1.A OptionValues
+              1.B OptionActions
+            2. OuterTableWrapper
+              2.A TableObject
+        */}
+        <Container>
+          <OptionValuesStruct/>
+          <OptionActionsStruct/>
+          <br/>
+          <QuoteTableStruct/>
+        </Container>
       </div>
     );
   }
