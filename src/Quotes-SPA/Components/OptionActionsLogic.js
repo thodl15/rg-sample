bb2846538ce3926b6ea56996b3d0d702cefcc7dc
David Thomas <thodl15@gmail.com>
2018-10-09 11:45:48 -0400

    Added Action logic component framework.

---
+++ src/Quotes-SPA/Components/OptionActionsLogic.js
@@ -0,0 +1,21 @@
+// ----------------------------------------------------------------------------
+// Core Imports:
+
+// React Imports:
+import React from 'react';
+
+// Structure Import:
+import OptionActionsStruct from './OptionActionsStruct';
+
+
+// ----------------------------------------------------------------------------
+// Module Exports:
+export default class OptionActionsLogic extends React.Component {
+    render() {
+        return (
+            <OptionActionsStruct
+
+            />
+        )
+    }
+}
