158908f47d633cabe28504823841f36a07340e9e
David Thomas <thodl15@gmail.com>
2018-10-09 10:32:31 -0400

    Added initial rate parameters component structure.

---
+++ src/Quotes-SPA/Components/OptionValuesStruct.js
@@ -0,0 +1,71 @@
+// ----------------------------------------------------------------------------
+// Core Imports:
+
+// React Imports:
+import React from 'react';
+
+
+// ----------------------------------------------------------------------------
+// Component Imports:
+
+// React-Bootstrap Imports:
+import {Row, Col, Form, FormGroup, Label, Input, FormText} from 'reactstrap';
+
+
+const OptionValuesStruct = (props) => {
+    return (
+        <Form>
+            <Row form>
+                <Col xs={12} sm={6}>
+                    <FormGroup row>
+                        <Col xs={4} sm={4}>
+                            Loan Size
+                        </Col>
+                        <Col xs={8} sm={7}>
+                            <Input type="text"/>
+                        </Col>
+                    </FormGroup>
+                    <br/>
+                    <FormGroup row>
+                        <Col xs={4} sm={4}>
+                            Credit Score
+                        </Col>
+                        <Col xs={8} sm={7}>
+                            <Input type="text"/>
+                        </Col>
+                    </FormGroup>
+                </Col>
+                <Col xs={12} sm={6}>
+                    <FormGroup row>
+                        <Col xs={4} sm={5}>
+                            Property Type
+                        </Col>
+                        <Col xs={8} sm={7}>
+                            <Input type="select">
+                                <option value="SingleFamily">Single Family</option>
+                                <option value="Condo">Condo</option>
+                                <option value="Townhouse">Townhouse</option>
+                                <option value="MultiFamily">Multi-Family</option>
+                            </Input>
+                        </Col>
+                    </FormGroup>
+                    <br/>
+                    <FormGroup row>
+                        <Col xs={4} sm={5}>
+                            Occupancy
+                        </Col>
+                        <Col xs={8} sm={7}>
+                            <Input type="select">
+                                <option value="Primary">Primary</option>
+                                <option value="Secondary">Secondary</option>
+                                <option value="Investment">Investment</option>
+                            </Input>
+                        </Col>
+                    </FormGroup>
+                </Col>
+            </Row>
+        </Form>
+    );
+}
+
+export default OptionValuesStruct;
