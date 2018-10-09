465d5e210cb20f9918de46fd7c2578e6d4e99709
David Thomas <thodl15@gmail.com>
2018-10-09 10:32:06 -0400

    Added initial datatable component structure.

---
+++ src/Quotes-SPA/Components/QuoteTableStruct.js
@@ -0,0 +1,54 @@
+import React from 'react';
+
+import BootstrapTable from 'react-bootstrap-table-next';
+
+import Col from 'react-bootstrap/lib/Col';
+import Row from 'react-bootstrap/lib/Row';
+
+const products = [ {
+    lenderName: "TFB Federal",
+    loanType: "10/1 ARM",
+    interestRate: 4.125,
+    closingCosts: 10000,
+    monthlyPayment: 2000,
+    apr: 4.375
+},{
+    lenderName: "TFB Federal",
+    loanType: "10/1 ARM",
+    interestRate: 4.125,
+    closingCosts: 10000,
+    monthlyPayment: 2000,
+    apr: 4.375
+} ];
+
+const columns = [{
+    dataField: 'lenderName',
+    text: 'Lender'
+}, {
+    dataField: 'loanType',
+    text: 'Product'
+}, {
+    dataField: 'interestRate',
+    text: 'Rate'
+}, {
+    dataField: 'closingCosts',
+    text: 'Closing Costs'
+}, {
+    dataField: 'monthlyPayment',
+    text: 'Monthly Payment'
+}, {
+    dataField: 'apr',
+    text: 'APR'
+}];
+
+const QuoteTableStruct = (props) => {
+    return (
+        <BootstrapTable
+            hover
+            condensed={true}
+            keyField='lenderName' 
+            data={ products } 
+            columns={ columns } />);
+}
+
+export default QuoteTableStruct;
