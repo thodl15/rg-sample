7b6dec4c12233a382e902c3ee9026c87d55c11f6
David Thomas <thodl15@gmail.com>
2018-10-09 13:40:45 -0400

    Added an action for storing the response, and their action definitions.

--- src/Quotes-SPA/Redux/Actions/ActionTypes.js
+++ src/Quotes-SPA/Redux/Actions/ActionTypes.js
@@ -1,7 +1,19 @@
 export const CHANGE_QUOTE_PARAMS = 'CHANGE_QUOTE_PARAMS'
+export const GET_QUOTE_LIST      = 'GET_QUOTE_LIST'
 
 export function changeQuoteParams(paramsObj) {
     return {
-        
+        type: CHANGE_QUOTE_PARAMS,
+        loanSize:     paramsObj.loanSize,
+        creditScore:  paramsObj.creditScore,
+        propertyType: paramsObj.propertyType,
+        occupancy:    paramsObj.occupancy,
+    }
+}
+
+export function getQuoteList(fetchRes) {
+    return {
+        type: GET_QUOTE_LIST,
+        quoteList: fetchRes.quoteList,
     }
 }
