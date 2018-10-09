bbb1cb6ae1f69936733bbd9bd2e1d20151ef4652
David Thomas <thodl15@gmail.com>
2018-10-09 13:42:59 -0400

    Added basic state/dispatch definitions and the redux wrapper.

--- src/Quotes-SPA/Components/OptionValuesLogic.js
+++ src/Quotes-SPA/Components/OptionValuesLogic.js
@@ -4,13 +4,36 @@
 // React Imports:
 import React from 'react';
 
+// React-Redux Imports:
+import { connect } from 'react-redux';
+import { changeQuoteParams } from '../Redux/Actions/ActionTypes';
+
 // Structure Import:
 import OptionValuesStruct from './OptionValuesStruct';
 
 
 // ----------------------------------------------------------------------------
+// Utility Functions & Constants:
+
+// React-Redux Action & Dispatch Functions:
+const mapStateToProps = (state, ownProps) => {
+    return {
+
+    }
+}
+
+const mapDispatchToProps = (dispatch, ownProps) => {
+    return {
+        sendQuoteParamsToStore: paramsObj => {
+            dispatch(changeQuoteParams(paramsObj))
+        }
+    }
+}
+
+
+// ----------------------------------------------------------------------------
 // Module Exports:
-export default class OptionValuesLogic extends React.Component {
+class OptionValuesLogic extends React.Component {
     render() {
         return (
             <OptionValuesStruct
@@ -19,3 +42,10 @@ export default class OptionValuesLogic extends React.Component {
         )
     }
 }
+
+const OptionValuesStore = connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(OptionValuesLogic);
+
+export default OptionValuesStore;
