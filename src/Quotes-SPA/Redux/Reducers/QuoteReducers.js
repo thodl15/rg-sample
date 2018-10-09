fb4e572f662a3d3e42a1c670e773e84c60b21bcd
David Thomas <thodl15@gmail.com>
2018-10-09 13:41:44 -0400

    Added initial values for the store, and definitions for reducers.

--- src/Quotes-SPA/Redux/Reducers/QuoteReducers.js
+++ src/Quotes-SPA/Redux/Reducers/QuoteReducers.js
@@ -2,13 +2,18 @@
 // Store Components:
 
 // Action Types:
-import { CHANGE_QUOTE_PARAMS } from './../Actions/ActionTypes.js';
+import { CHANGE_QUOTE_PARAMS,
+         GET_QUOTE_LIST }     from './../Actions/ActionTypes.js';
 
 
 // ----------------------------------------------------------------------------
 // Utility Functions & Constants:
 const initialState = {
-    
+    loanSize:     0,
+    creditScore:  0,
+    propertyType: SingleFamily,
+    occupancy:    Primary,
+    quoteList:    [],
 }
 
 
@@ -18,7 +23,14 @@ export default function paramsState(state = initialState, action) {
     switch(action.type) {
         case CHANGE_QUOTE_PARAMS:
             return Object.assign({}, state, {
-
+                loanSize:     action.loanSize,
+                creditScore:  action.creditScore,
+                propertyType: action.propertyType,
+                occupancy:    action.occupancy,
+            })
+        case GET_QUOTE_LIST:
+            return Object.assign({}, state, {
+                quoteList: action.quoteList,
             })
         default:
             return state
