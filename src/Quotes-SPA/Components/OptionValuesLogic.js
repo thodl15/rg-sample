74e0b6c8c88673d36cb48baa0a856acb837198c8
David Thomas <thodl15@gmail.com>
2018-10-09 11:46:00 -0400

    Added quote parameters logic component framework.

---
+++ src/Quotes-SPA/Components/OptionValuesLogic.js
@@ -0,0 +1,21 @@
+// ----------------------------------------------------------------------------
+// Core Imports:
+
+// React Imports:
+import React from 'react';
+
+// Structure Import:
+import OptionValuesStruct from './OptionValuesStruct';
+
+
+// ----------------------------------------------------------------------------
+// Module Exports:
+export default class OptionValuesLogic extends React.Component {
+    render() {
+        return (
+            <OptionValuesStruct
+
+            />
+        )
+    }
+}
