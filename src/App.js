e1cee0c76e517d5d0fd69a2666a5159f16546f06
David Thomas <thodl15@gmail.com>
2018-10-09 19:50:09 -0400

    Organizing imports and adding comments for improved clarity.

--- src/App.js
+++ src/App.js
@@ -1,19 +1,26 @@
+// ----------------------------------------------------------------------------
+// Core Imports:
+
+// React Imports:
 import React, { Component } from 'react';
 
+// React-Redux Imports:
 import { createStore } from 'redux';
 import { Provider    } from 'react-redux';
+import paramsState     from './Quotes-SPA/Redux/Reducers/QuoteReducers';
 
-import paramsState from './Quotes-SPA/Redux/Reducers/QuoteReducers';
-
+// Style Imports:
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
+
+// Component Imports:
 import OptionActionsStore from './Quotes-SPA/Components/OptionActionsLogic';
 import OptionValuesStore  from './Quotes-SPA/Components/OptionValuesLogic';
 import QuoteTableLogic    from './Quotes-SPA/Components/QuoteTableLogic';
+import { Container      } from 'reactstrap';
 
-import {Container} from 'reactstrap';
-
+// Redux store initialization:
 const store = createStore(paramsState);
 
 class App extends Component {
